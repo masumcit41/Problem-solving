11495f5610aa91e75dec18bb292732ede7e4f955
Masum Ahmed <93674729+masumcit41@users.noreply.github.com>
2022-06-29 14:39:40 +0600

    Add files via upload

---
+++ 1002/URI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+         double R , A;
+         R = Convert.ToDouble(Console.ReadLine());
+        
+         A = 3.1416 * ( R * R);
+         Console.WriteLine("A=" + A.ToString("0.0000"));
+         Console.ReadKey();
+    }
+
+}
